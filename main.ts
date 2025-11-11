e110bdb2b9df24c2a06f80f5cfb014fadcb0c134
Botirocky <buczibotond@gmail.com>
2025-11-11 20:30:50 +0100

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,13 +1,100 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 1; index++) {
+        music.play(music.stringPlayable("C5 A F D - - - - ", 130), music.PlaybackMode.UntilDone)
+        basic.showString("M")
+        basic.showLeds(`
+            . . . # .
+            . . # . .
+            . # . # .
+            # # # # #
+            # . . . #
+            `)
+        basic.showString("v")
+        basic.pause(2000)
+        basic.showLeds(`
+            . . # # .
+            # # . # #
+            . . . # .
+            # # . # #
+            . . # # .
+            `)
+        basic.pause(1000)
+        basic.showString("kedves utasaink a vonat kesik 5 percet")
+    }
+    for (let index = 0; index < 1; index++) {
+        music.play(music.stringPlayable("C5 A F D - - - - ", 130), music.PlaybackMode.UntilDone)
+        basic.showString("M")
+        basic.showLeds(`
+            . . . # .
+            . . # . .
+            . # . # .
+            # # # # #
+            # . . . #
+            `)
+        basic.showString("v")
+        basic.pause(2000)
+        basic.showLeds(`
+            . . # # .
+            # # . # #
+            . . . # .
+            # # . # #
+            . . # # .
+            `)
+        basic.pause(1000)
+        basic.showString("kedves utasaink a vonat kesik 10 percet")
+    }
+    for (let index = 0; index < 1; index++) {
+        music.play(music.stringPlayable("C5 A F D - - - - ", 130), music.PlaybackMode.UntilDone)
+        basic.showString("M")
+        basic.showLeds(`
+            . . . # .
+            . . # . .
+            . # . # .
+            # # # # #
+            # . . . #
+            `)
+        basic.showString("v")
+        basic.pause(2000)
+        basic.showLeds(`
+            . . # # .
+            # # . # #
+            . . . # .
+            # # . # #
+            . . # # .
+            `)
+        basic.pause(1000)
+        basic.showString("kedves utasaink a vonat kesik 15 percet")
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("the code was made by Boti")
+})
 let hőmérséklet = 0
 let érték = 0
 music.play(music.stringPlayable("- - A G F E D - ", 190), music.PlaybackMode.InBackground)
 basic.showString("Mr.Robot")
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     if (true) {
         érték = 23
     }
-    
     hőmérséklet += 2
     while (false) {
         music.play(music.stringPlayable("C5 B C5 B C5 B C5 B ", 500), music.PlaybackMode.UntilDone)
